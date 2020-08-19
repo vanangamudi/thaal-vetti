d2fe91fa6e928e4c93b9f693205a4c35056a33b1
vanangamudi <selva.developer@gmail.com>
2020-08-19 12:58:01 +0530

    Internal commands are now configurable via variables
    config variables are now defined at the top of the script.

--- vetti.sh
+++ vetti.sh
@@ -1,18 +1,38 @@
+#### configs
+# Chunking config
+PAGE_RESOLUTION=150
 
-COMMAND=$1 #csm
+# vetti.py scale factor, if the window appears too big decrease this number
+DISPLAY_RESOLUTION=0.5
+DEBUG='-D' # for debugging make it '--debug'
+CLEAR='' # for clearing old state make it '--clear',
+           #if you change display resolution set this to '--clear'
+
+
+#### end of configs ####
+
+
+COMMAND=$1 #csm similar to chmod rwx
 INPUT=$2
 
 INPUT_PAGES_DIR=${INPUT}_pages
 INPUT_SPLIT_PAGES_DIR=${INPUT}_split_pages
 
-mkdir -p $INPUT_PAGES_DIR $INPUT_SPLIT_PAGES_DIR
+mkdir -p $INPUT_PAGES_DIR
 
 if [[ $COMMAND == *"c"* ]]
 then
     echo "chunking pdf file into pages"
     # remove PNG24: and add necessary info into problems and fixes file
     # convert -set colorspace RGB -density 300 $INPUT PNG24:$INPUT_PAGES_DIR/%04d.png
-    gs  -dBATCH -dNOPAUSE -dSAFER -sDEVICE=png16m  -dJPEGQ=95 -r300x300 -sOutputFile=$INPUT_PAGES_DIR/%04d.png  $INPUT
+    gs  -dBATCH \
+	-dNOPAUSE \
+	-dSAFER \
+	-sDEVICE=png16m  \
+	-dJPEGQ=95 \
+	-r${PAGE_RESOLUTION}x${PAGE_RESOLUTION} \
+	-sOutputFile=$INPUT_PAGES_DIR/%04d.png \
+	$INPUT
     
     RET=$?
     if test $RET -ne 0
@@ -34,8 +54,14 @@ then
 
     # per page processing, in this case just copying the pages
     echo "doing the thing"
+    mkdir -p  $INPUT_SPLIT_PAGES_DIR
     #cp $INPUT_PAGES_DIR/* $INPUT_SPLIT_PAGES_DIR
-    python3 vetti.py -i $INPUT_PAGES_DIR -o $INPUT_SPLIT_PAGES_DIR
+    python3 vetti.py \
+	    --display-resolution $DISPLAY_RESOLUTION \
+	    $DEBUG \
+	    $CLEAR \
+	    -i $INPUT_PAGES_DIR \
+	    -o $INPUT_SPLIT_PAGES_DIR
     
     RET=$?
     if test $RET -ne 0
